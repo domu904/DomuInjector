b41bdfadd6f08cb856302656dacd1f5ad55c1d46
domu904 <domu904@gmail.com>
2014-01-19 17:58:50 -0600

    Initial commit for DomuInjector

---
+++ Diary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomuInjector
+{
+    class Diary
+    {
+        private string clock;
+        private DateTime date;
+
+        public Diary(string clock, DateTime date)
+        {
+            this.clock = clock;
+            this.date = date;
+        }
+
+    }    
+}
